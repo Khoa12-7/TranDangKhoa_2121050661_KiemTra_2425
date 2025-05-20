48d3a85d5f5b0f960c702782d9fd73236145d4b7
Khoa12-7 <Trandangkhoa12072003@gmail.com>
2025-05-20 09:36:51 +0700

    Bài Kiểm Tra B

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TranDangKhoa_2121050661_KiemTra_2425.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
